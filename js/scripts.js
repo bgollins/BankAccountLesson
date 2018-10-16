9135b2d8b6faed3cc031321b4d39d6abf1e5cf65
Brittany Gollins and Charley McGowan <brittany@>
2018-10-15 18:55:18 -0700

    added a way to add and store multiple bank accounts

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,16 @@ function BankAccount(name, initialDeposit){
 }
 
 
-BankAccount.prototype.withdrawal = function(withdrawalAmount){
-  return currentBalance - withdrawalAmount;
+BankAccount.prototype.withdrawal = function(withdrawalAmount, index){
+  return BankAccount.currentBalance - withdrawalAmount;
 }
 
-BankAccount.prototype.withdrawal = function(depositAmount){
-  return currentBalance + depositAmount;
+BankAccount.prototype.withdrawal = function(depositAmount, index){
+  return BankAccounts[index].currentBalance + depositAmount;
 }
+
+var bankAccounts = [];
+var bankAccountsIndex = 0;
 
 // UI logic
 $(document).ready(function() {
@@ -19,11 +22,12 @@ $(document).ready(function() {
       event.preventDefault();
     var submittedName = $("input#NewName").val();
     var submittedInitialAmount = parseInt($("input#InitialDeposit").val());
-
     var newBankAccount = new BankAccount(submittedName, submittedInitialAmount);
-    console.log("This is the name:" + newBankAccount.name);
-    console.log("This is the initial deposit: " + newBankAccount.currentBalance);
 
+    console.log("This is the name: " + newBankAccount.name);
+    console.log("This is the initial deposit: " + newBankAccount.currentBalance);
+    bankAccounts.push(newBankAccount);
+    console.log(newBankAccount);
   });
 
 });
