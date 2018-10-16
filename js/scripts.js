165444ca75116cb4331ba591a345c9b8350105d4
Brittany Gollins and Charley McGowan <brittany@>
2018-10-15 18:36:42 -0700

    submit button has started functioning

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,29 @@
 // Busines logic
 function BankAccount(name, initialDeposit){
-  this.name = [];
-  this.initialDeposit = initialDeposit;
+  this.name = name;
+  this.currentBalance = initialDeposit;
 }
 
-BankAccount.prototype.withdrawal() = function(withdrawalAmount){
-  return initialDeposit - withdrawalAmount;
+
+BankAccount.prototype.withdrawal = function(withdrawalAmount){
+  return currentBalance - withdrawalAmount;
+}
+
+BankAccount.prototype.withdrawal = function(depositAmount){
+  return currentBalance + depositAmount;
 }
 
 // UI logic
-$(document).ready(function(event){
-  event.preventDefault();
-  console.log("I'm Ready");
-  
+$(document).ready(function() {
+  $("form.contact").submit(function(event) {
+      event.preventDefault();
+    var submittedName = $("input#NewName").val();
+    var submittedInitialAmount = parseInt($("input#InitialDeposit").val());
+
+    var newBankAccount = new BankAccount(submittedName, submittedInitialAmount);
+    console.log("This is the name:" + newBankAccount.name);
+    console.log("This is the initial deposit: " + newBankAccount.currentBalance);
+
+  });
+
 });
