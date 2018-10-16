ff4d78efd3cad4977a0f5db83ebebebf0770690e
Brittany Gollins and Charley McGowan <brittany@>
2018-10-15 18:07:46 -0700

    Initial pair commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 // Busines logic
+function BankAccount(name, initialDeposit){
+  this.name = [];
+  this.initialDeposit = initialDeposit;
+}
 
+BankAccount.prototype.withdrawal() = function(withdrawalAmount){
+  return initialDeposit - withdrawalAmount;
+}
 
 // UI logic
 $(document).ready(function(event){
   event.preventDefault();
   console.log("I'm Ready");
+  
 });
